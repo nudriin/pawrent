09b62a938cf854459ab19df3f252206186765c2b
Nurdin <mrsunnysummer@gmail.com>
2025-02-18 01:30:11 +0700

    chore: add shadcn dep

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/ui/button"
 export default function Home() {
-    return <div className="text-blue-500">Hello</div>
+    return (
+        <div className="text-blue-500">
+            <Button>Hello</Button>
+        </div>
+    )
 }
