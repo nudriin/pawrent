1a35c8180cdc8514d36e52e72894b05aaea55b85
Nurdin <mrsunnysummer@gmail.com>
2025-02-18 15:28:35 +0700

    feat: create register functionality

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -9,8 +9,42 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export function Register() {
+    const [formData, setFormData] = useState({})
+    const navigate = useNavigate()
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        })
+        console.log(formData)
+    }
+
+    const handleRegister = async (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault()
+        try {
+            const response = await fetch("/api/pawrent", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            })
+
+            const body = await response.json()
+
+            console.log(body)
+
+            navigate("/login")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div className="flex items-center justify-center flex-col min-h-screen max-h-screen">
             <Card className="w-[350px] shadow-2xl">
@@ -24,18 +58,37 @@ export function Register() {
                     <form>
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="name">Name</Label>
-                                <Input id="name" placeholder="John Doe" />
+                                <Label htmlFor="nama_lengkap_pawrent">
+                                    Name
+                                </Label>
+                                <Input
+                                    onChange={handleChange}
+                                    id="nama_lengkap_pawrent"
+                                    placeholder="John Doe"
+                                />
                             </div>
                             <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="phone">Phone</Label>
-                                <Input id="phone" placeholder="0829110****" />
+                                <Label htmlFor="no_telepon_pawrent">
+                                    Phone
+                                </Label>
+                                <Input
+                                    onChange={handleChange}
+                                    id="no_telepon_pawrent"
+                                    placeholder="0829110****"
+                                    type="tel"
+                                />
+                            </div>
+                            <div className="flex flex-col space-y-1.5">
+                                <Label htmlFor="password">Password</Label>
+                                <Input id="password" placeholder="*****" />
                             </div>
                         </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex w-full">
-                    <Button className="w-full">Register</Button>
+                    <Button onClick={handleRegister} className="w-full">
+                        Register
+                    </Button>
                 </CardFooter>
             </Card>
         </div>
