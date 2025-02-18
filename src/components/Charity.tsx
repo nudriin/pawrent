308f22ef3eed4186b17fc354fa09f617e22d0cac
Nurdin <mrsunnysummer@gmail.com>
2025-02-18 11:31:43 +0700

    style: create charity section

---
+++ src/components/Charity.tsx
@@ -0,0 +1,33 @@
+export default function Charity() {
+    return (
+        <div className="text-white px-6 py-4 bg-paw">
+            <div className="">
+                <h1 className="text-2xl font-medium mb-10 mt-4">
+                    We collaborate with charities around the world
+                </h1>
+                <div className="grid grid-cols-4 gap-4">
+                    <p className="col-span-2 bg-paw-secondary px-6 py-4 rounded-xl text-lg">
+                        Our vet clinic prioritizes community involvement and
+                        animal welfare through regular charity events benefiting
+                        local animal shelters, rescue organizations, and related
+                        causes
+                    </p>
+                    <p className="col-span-1 bg-paw-secondary px-6 py-4 rounded-xl text-lg">
+                        <span className="text-4xl font-bold">110+</span>
+                        <br /> Animal saved all time
+                    </p>
+                    <p className="col-span-1 bg-paw-secondary px-6 py-4 rounded-xl text-lg">
+                        <span className="text-4xl font-bold">64</span>
+                        <br />
+                        Charitable events held
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+//We collaborate with charities around the world
+
+//Our vet clinic prioritizes community involvement and animal welfare through regular charity events benefiting local animal shelters, rescue organizations, and related causes
+
+//Collaborate with popular brand
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import About from "@/components/About"
+import Charity from "@/components/Charity"
 import Header from "@/components/Header"
 import Hero from "@/components/Hero"
 export default function Home() {
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="max-w-screen">
             <Header />
             <Hero />
-            <About />
+            <Charity />
         </div>
     )
 }
